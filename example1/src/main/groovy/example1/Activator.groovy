c893180889d558f7e334612e5486cb95671406b5
Mario Garcia <mario.garcia@kaleidos.net>
2017-03-03 19:32:55 +0100

    Adding more felix tutorial examples

--- example1/src/main/groovy/example1/Activator.groovy
+++ example1/src/main/groovy/example1/Activator.groovy
@@ -10,17 +10,16 @@ import org.osgi.framework.ServiceEvent
  * framework's event mechanism to listen for service events. Upon
  * receiving a service event, it prints out the event's details.
 **/
-class Activator implements BundleActivator, ServiceListener
-{
+class Activator implements BundleActivator, ServiceListener {
+
     /**
      * Implements BundleActivator.start(). Prints
      * a message and adds itself to the bundle context as a service
      * listener.
      * @param context the framework context for the bundle.
     **/
-    void start(BundleContext context)
-    {
-        println("Starting to listen for service events.")
+    void start(BundleContext context) {
+        println "Starting to listen for service events."
         context.addServiceListener(this)
     }
 
@@ -30,13 +29,9 @@ class Activator implements BundleActivator, ServiceListener
      * service listener.
      * @param context the framework context for the bundle.
     **/
-    void stop(BundleContext context)
-    {
+    void stop(BundleContext context) {
         context.removeServiceListener(this)
-        println("Stopped listening for service events.")
-
-        // Note: It is not required that we remove the listener here,
-        // since the framework will do it automatically anyway.
+        println "Stopped listening for service events."
     }
 
     /**
@@ -44,25 +39,19 @@ class Activator implements BundleActivator, ServiceListener
      * Prints the details of any service event from the framework.
      * @param event the fired service event.
     **/
-    void serviceChanged(ServiceEvent event)
-    {
-        String[] objectClass = (String[])
-            event.getServiceReference().getProperty("objectClass")
+    void serviceChanged(ServiceEvent event) {
+        String[] objectClass = (String[]) event.getServiceReference().getProperty("objectClass")
 
-        if (event.getType() == ServiceEvent.REGISTERED)
-        {
-            println(
-                "Ex1: Service of type " + objectClass[0] + " registered.")
-        }
-        else if (event.getType() == ServiceEvent.UNREGISTERING)
-        {
-            println(
-                "Ex1: Service of type " + objectClass[0] + " unregistered.")
-        }
-        else if (event.getType() == ServiceEvent.MODIFIED)
-        {
-            println(
-                "Ex1: Service of type " + objectClass[0] + " modified.")
+        switch(event.type) {
+            case ServiceEvent.REGISTERED:
+            println "Ex1: Service of type ${objectClass.first()} registered."
+            break
+            case ServiceEvent.UNREGISTERING:
+            println "Ex1: Service of type ${objectClass.first()} unregistered."
+            break
+            case ServiceEvent.MODIFIED:
+            println "Ex1: Service of type ${objectClass.first()} modified."
+            break
         }
     }
 }
